a522e20fe981e3a11de67fb21901ae60ae7747aa
Michael Novi <me@michaelnovi.com>
2016-08-15 13:10:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This creates a Virtual Host-based Reverse Proxy for multiple sites/services, building configuration files, on the fly.
+
+  domain1:master_port=localhost:service_port;domain2:master_port=localhost:service_port
---
+++ startup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# domain1:master_port=localhost:service_port;domain2:master_port=localhost:service_port
+function create_site_config {
+  site_proxies=$(echo $1 | tr ";" "\n")
+
+  for site_proxy in $site_proxies
+  do
+    IFS=";"
+    proxy_map=($site_proxy)
+    IFS="="
+    proxy_map_array=($proxy_map)
+    IFS=":"
+    inbound=(${proxy_map_array[0]})
+    
+    IFS=" "
+    cat > /etc/nginx/sites-available/${inbound[0]}.conf <<EOF
+server {
+  listen ${inbound[1]}
+  server ${inbound[0]}
+
+  location / {
+    proxypass http://${proxy_map_array[1]}
+  }
+}
+EOF
+
+    ln -s /etc/nginx/sites-available/${inbound[0]}.conf /etc/nginx/sites-enabled/${inbound[0]}.conf
+  done
+}
+
+create_site_config $SITE_PROXIES
+nginx -g 'daemon off;'
